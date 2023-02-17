4e2e43d231434bc2d622b42deedb5dec475372d3
Josh Ribb <joshribb@rightnow.org>
2023-02-16 21:03:47 -0600

    re-add files back

---
+++ src/components/Clock.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+    import { onDestroy } from 'svelte';
+    import dayjs from 'dayjs';
+
+    let now: string;
+    let timeout: number;
+
+    (function tick() {
+        now = dayjs().format('MMMM D, YYYY h:mm A');
+        timeout = setTimeout(tick, 1000);
+    })();
+
+    onDestroy(() => {
+        clearTimeout(timeout);
+    });
+</script>
+
+<p>{now}</p>
+
+<style>
+    p {
+        font-size: .75em;
+        opacity: 0.5;
+    }
+</style>
